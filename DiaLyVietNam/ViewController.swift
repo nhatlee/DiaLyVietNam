96c42a3e94b805f65d4f99b79a0f3e5a272ad676
nhatlee <lenhathk@gmail.com>
2017-10-24 08:36:54 +0700

    initial app

---
+++ DiaLyVietNam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DiaLyVietNam
+//
+//  Created by nhatlee on 10/24/17.
+//  Copyright © 2017 nhatlee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
